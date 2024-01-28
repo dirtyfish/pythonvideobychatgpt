cfde415935d3711a87485eeb751c069fd10b24f2
Espen <espenvh@gmail.com>
2024-01-28 21:30:54 +0100

    Update myvid.py

--- myvid.py
+++ myvid.py
@@ -28,7 +28,7 @@ try:
     subprocess.run(['pip', 'install', 'opencv-python', 'numpy'])
 except Exception as e:
     print(f"An error occurred during package installation: {e}")
-    # exit()
+
 
 import cv2
 import numpy as np
